6e64cc968866dd7d12f96f82d7f260a4ad01732d
Nilupul Madhawa <68707780+nilupulmadhawa@users.noreply.github.com>
2023-08-20 21:31:39 +0530

    update

--- hna0s9ce_saaims.sql
+++ hna0s9ce_saaims.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hna0s9ce_saaims`
+-- Database: `zigmpelb_saaims`
 --
 
 -- --------------------------------------------------------
